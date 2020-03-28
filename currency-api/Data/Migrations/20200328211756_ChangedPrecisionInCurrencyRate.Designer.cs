2ccf65c9d9918a3aa6a6ae8ec459b58e27581b27
Damianos Pappas <dam.pappas@gmail.com>
2020-03-28 23:45:48 +0200

    Changed CurrencyRate rate Precision

--- currency-api/Data/Migrations/20200328211756_ChangedPrecisionInCurrencyRate.Designer.cs
+++ currency-api/Data/Migrations/20200328211756_ChangedPrecisionInCurrencyRate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using currencyApi.Data;
 
 namespace currency_api.Migrations
 {
     [DbContext(typeof(CurrenciesContext))]
-    partial class CurrenciesContextModelSnapshot : ModelSnapshot
+    [Migration("20200328211756_ChangedPrecisionInCurrencyRate")]
+    partial class ChangedPrecisionInCurrencyRate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ currency-api/Data/Migrations/20200328211756_ChangedPrecisionInCurrencyRate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace currency_api.Migrations
+{
+    public partial class ChangedPrecisionInCurrencyRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "CurrencyRates",
+                type: "decimal(18, 10)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rate",
+                table: "CurrencyRates",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 10)");
+
+        }
+    }
+}
