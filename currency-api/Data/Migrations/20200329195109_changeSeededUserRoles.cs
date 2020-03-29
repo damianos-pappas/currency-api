cb178f8dfd1dc9e4f688f64225bc63fcfd75d331
Damianos Pappas <dam.pappas@gmail.com>
2020-03-29 23:26:59 +0300

    Change seeded User Roles

---
+++ currency-api/Data/Migrations/20200329195109_changeSeededUserRoles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace currency_api.Migrations
+{
+    public partial class changeSeededUserRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.UpdateData(
+                table: "UserRoleRelations",
+                keyColumn: "Id",
+                keyValue: 3L,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2020, 3, 29, 19, 51, 8, 999, DateTimeKind.Utc).AddTicks(9426), 1L });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoleRelations",
+                keyColumn: "Id",
+                keyValue: 4L,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2020, 3, 29, 19, 51, 8, 999, DateTimeKind.Utc).AddTicks(9428), 1L });
+
+            migrationBuilder.UpdateData(
+                table: "UserRoleRelations",
+                keyColumn: "Id",
+                keyValue: 5L,
+                columns: new[] { "CreatedAt", "UserId" },
+                values: new object[] { new DateTime(2020, 3, 29, 19, 51, 8, 999, DateTimeKind.Utc).AddTicks(9429), 1L });
+
+                      
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+         
+        }
+    }
+}
