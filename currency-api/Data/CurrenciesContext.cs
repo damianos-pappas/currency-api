ae3a56fb0856822a1b6e61e9696184efee0f29ec
Damianos Pappas <dam.pappas@gmail.com>
2020-03-29 16:59:55 +0300

    Changed User passwords to be encrypted in initial data seeding

--- currency-api/Data/CurrenciesContext.cs
+++ currency-api/Data/CurrenciesContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using currencyApi.BusinessLogic.Helpers;
 using currencyApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory.ValueGeneration.Internal;
@@ -41,7 +42,7 @@ namespace currencyApi.Data
                     entity.HasQueryFilter(r => r.IsDeleted == false);
 
                     if (Database.IsInMemory())
-                            entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
+                        entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
 
                     entity.HasIndex(c => c.Code).IsUnique();
 
@@ -66,7 +67,7 @@ namespace currencyApi.Data
                     entity.HasQueryFilter(r => r.IsDeleted == false);
 
                     if (Database.IsInMemory())
-                            entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
+                        entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
 
                     entity.HasIndex(r => new { r.BaseCurrencyId, r.TargetCurrencyId })
                           .IsUnique();
@@ -79,7 +80,7 @@ namespace currencyApi.Data
                           .WithMany()
                           .HasForeignKey(r => r.TargetCurrencyId);
 
-                     entity.Property(r =>r.Rate).HasColumnType("decimal(18, 10)");
+                    entity.Property(r => r.Rate).HasColumnType("decimal(18, 10)");
                 }
             );
 
@@ -95,7 +96,7 @@ namespace currencyApi.Data
                     entity.HasQueryFilter(r => r.IsDeleted == false);
 
                     if (Database.IsInMemory())
-                            entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
+                        entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
                 }
             );
 
@@ -108,8 +109,10 @@ namespace currencyApi.Data
 
                     entity.Property(r => r.Id).ValueGeneratedOnAdd();
 
+                    entity.HasIndex(r => r.Description).IsUnique();
+
                     if (Database.IsInMemory())
-                            entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
+                        entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
                 }
             );
 
@@ -125,7 +128,7 @@ namespace currencyApi.Data
                     entity.HasQueryFilter(r => r.IsDeleted == false);
 
                     if (Database.IsInMemory())
-                            entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
+                        entity.Property(c => c.Id).HasValueGenerator<InMemoryIntegerValueGenerator<long>>();
 
                     entity.HasIndex(r => new { r.UserId, r.RoleId })
                           .IsUnique();
@@ -140,65 +143,84 @@ namespace currencyApi.Data
                 }
             );
 
-        /*******************************************************************
-            DATA SEEDING
-        ********************************************************************/
-             modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, UserName = "admin", Email = "user@currencies.cur", PasswordHash="xxx",  CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new User { Id = 2, UserName = "user", Email = "user@currencies.cur", PasswordHash="xxx",CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false }
-            );
+            /*******************************************************************
+                DATA SEEDING
+            ********************************************************************/
+            modelBuilder.Entity<User>().HasData(
+               new User
+               {
+                   Id = 1,
+                   UserName = "admin",
+                   Email = "user@currencies.cur",
+                   PasswordHash = EncryptionHelper.Encrypt("admin123", "this_is_a_demo_project"),
+                   CreatedAt = DateTime.UtcNow,
+                   IsActive = true,
+                   IsDeleted = false
+               },
+               new User
+               {
+                   Id = 2,
+                   UserName = "user",
+                   Email = "user@currencies.cur",
+                   PasswordHash = EncryptionHelper.Encrypt("user123", "this_is_a_demo_project"),
+                   CreatedAt = DateTime.UtcNow,
+                   IsActive = true,
+                   IsDeleted = false
+               }
+           );
 
-             modelBuilder.Entity<UserRole>().HasData(
-                new UserRole { Id = 1, Description = "USER_CURRENCIES_RATES"},
-                new UserRole { Id = 2, Description = "ADMIN_CURRENCIES"},
-                new UserRole { Id = 3, Description = "ADMIN_RATES"},
-                new UserRole { Id = 4, Description = "ADMIN_USERS"}
-            );
+            modelBuilder.Entity<UserRole>().HasData(
+               new UserRole { Id = 1, Description = "USER_CURRENCIES_RATES" },
+               new UserRole { Id = 2, Description = "ADMIN_CURRENCIES" },
+               new UserRole { Id = 3, Description = "ADMIN_RATES" },
+               new UserRole { Id = 4, Description = "ADMIN_USERS" }
+           );
 
             modelBuilder.Entity<UserRoleRelation>().HasData(
-                new UserRoleRelation { Id = 1, UserId = 1, RoleId = 1, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new UserRoleRelation { Id = 2, UserId = 2, RoleId = 1, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new UserRoleRelation { Id = 3, UserId = 2, RoleId = 2, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new UserRoleRelation { Id = 4, UserId = 2, RoleId = 3, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new UserRoleRelation { Id = 5, UserId = 2, RoleId = 4, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false }
+                new UserRoleRelation { Id = 1, UserId = 1, RoleId = 1, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new UserRoleRelation { Id = 2, UserId = 2, RoleId = 1, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new UserRoleRelation { Id = 3, UserId = 2, RoleId = 2, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new UserRoleRelation { Id = 4, UserId = 2, RoleId = 3, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new UserRoleRelation { Id = 5, UserId = 2, RoleId = 4, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false }
             );
 
             modelBuilder.Entity<Currency>().HasData(
-                new Currency { Id = 1, Code = "EUR", Description = "Euro", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new Currency { Id = 2, Code = "USD", Description = "US Dollar", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new Currency { Id = 3, Code = "CHF", Description = "Swiss Franc", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new Currency { Id = 4, Code = "GBP", Description = "British Pound", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new Currency { Id = 5, Code = "JPY", Description = "Japan Yen", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new Currency { Id = 6, Code = "CAD", Description = "Canadian Dollar", CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  }
+                new Currency { Id = 1, Code = "EUR", Description = "Euro", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new Currency { Id = 2, Code = "USD", Description = "US Dollar", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new Currency { Id = 3, Code = "CHF", Description = "Swiss Franc", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new Currency { Id = 4, Code = "GBP", Description = "British Pound", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new Currency { Id = 5, Code = "JPY", Description = "Japan Yen", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new Currency { Id = 6, Code = "CAD", Description = "Canadian Dollar", CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false }
             );
 
             modelBuilder.Entity<CurrencyRate>().HasData(
-                new CurrencyRate { Id=1, BaseCurrencyId = 1, TargetCurrencyId = 2, Rate = 1.3764m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new CurrencyRate { Id=2, BaseCurrencyId = 1, TargetCurrencyId = 3, Rate = 1.2079m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new CurrencyRate { Id=3, BaseCurrencyId = 1, TargetCurrencyId = 4, Rate = 0.8731m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false },
-                new CurrencyRate { Id=4, BaseCurrencyId = 2, TargetCurrencyId = 5, Rate = 76.7200m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new CurrencyRate { Id=5, BaseCurrencyId = 3, TargetCurrencyId = 2, Rate = 1.1379m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  },
-                new CurrencyRate { Id=6, BaseCurrencyId = 4, TargetCurrencyId = 6, Rate = 1.5648m, CreatedAt= DateTime.UtcNow, IsActive=true, IsDeleted=false  }
+                new CurrencyRate { Id = 1, BaseCurrencyId = 1, TargetCurrencyId = 2, Rate = 1.3764m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new CurrencyRate { Id = 2, BaseCurrencyId = 1, TargetCurrencyId = 3, Rate = 1.2079m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new CurrencyRate { Id = 3, BaseCurrencyId = 1, TargetCurrencyId = 4, Rate = 0.8731m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new CurrencyRate { Id = 4, BaseCurrencyId = 2, TargetCurrencyId = 5, Rate = 76.7200m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new CurrencyRate { Id = 5, BaseCurrencyId = 3, TargetCurrencyId = 2, Rate = 1.1379m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false },
+                new CurrencyRate { Id = 6, BaseCurrencyId = 4, TargetCurrencyId = 6, Rate = 1.5648m, CreatedAt = DateTime.UtcNow, IsActive = true, IsDeleted = false }
             );
         }
 
         /*******************************************************************
             SaveChanges Enrichment
         ******************************************************************/
-            public override int SaveChanges()
+        public override int SaveChanges()
+        {
+            var entities = ChangeTracker.Entries()
+                        .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified)
+                        .Select(e => e.Entity);
+
+            foreach (var entity in entities)
+            {
+                if (entity is IValidateable validateableEntity)
                 {
-                    var entities = ChangeTracker.Entries()
-                                .Where(e => e.State == EntityState.Added|| e.State == EntityState.Modified)
-                                .Select(e => e.Entity);
-
-                    foreach (var entity in entities)
-                    {
-                       if(entity is IValidateable validateableEntity){
-                           validateableEntity.Validate();
-                       }
-                    }
-
-                    return base.SaveChanges();
+                    validateableEntity.Validate();
                 }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
---
+++ currency-api/Data/Migrations/20200329125505_SeedUserPasswordsHashed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace currency_api.Migrations
+{
+    public partial class SeedUserPasswordsHashed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2020, 3, 29, 12, 55, 5, 217, DateTimeKind.Utc).AddTicks(4587), "fGt1vKQUR082ue1S9ZKG5w==" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2L,
+                columns: new[] { "CreatedAt", "PasswordHash" },
+                values: new object[] { new DateTime(2020, 3, 29, 12, 55, 5, 219, DateTimeKind.Utc).AddTicks(6860), "nsoIb8ZBL/MwmYsc7adypQ==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
