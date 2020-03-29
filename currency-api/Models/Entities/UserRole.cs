aa6c623215a148efb1907417825619dd0a8a4401
Damianos Pappas <dam.pappas@gmail.com>
2020-03-29 15:41:33 +0300

    Enums in seperate file

--- currency-api/Models/Entities/UserRole.cs
+++ currency-api/Models/Entities/UserRole.cs
@@ -5,17 +5,4 @@ namespace currencyApi.Models
     public class UserRole : BaseLookup
     {
     }
-
-    public enum UserRoleEnum 
-    {
-        USER_CURRENCIES_RATES = 1,
-
-        ADMIN_CURRENCIES = 2,
-
-        ADMIN_RATES = 3,
-
-        ADMIN_USERS = 4,
-
-    }
-    
 }
---
+++ currency-api/Models/Enums.cs
@@ -0,0 +1,7 @@
+    public enum UserRoleEnum 
+    {
+        USER_CURRENCIES_RATES = 1,
+        ADMIN_CURRENCIES = 2,
+        ADMIN_RATES = 3,
+        ADMIN_USERS = 4,
+    }
